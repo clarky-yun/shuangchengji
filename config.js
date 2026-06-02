b66fb84b4737380ce68f3692774f40b595e6d494
clarky-yun <189867904+clarky-yun@users.noreply.github.com>
2026-06-02 20:49:06 +0800

    Configure Supabase project

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.DC_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://mgykdwioaatpdtotcfjj.supabase.co",
+  supabaseAnonKey: "sb_publishable_QJi06kM0Yi-xaYhaCFFFKw_Rp0xL-31",
   spaceId: "default",
 };
